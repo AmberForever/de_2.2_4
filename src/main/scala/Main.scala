992bd5c34393c12df7d1744e91afbb0168aaaa66
Галицкий Денис Андреевич <galitskiy.d.a@avtomir.ru>
2022-11-02 00:21:14 +0300

    3_a

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,22 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    var s = "Hello, Scala!"
+    println(s) // задача 2.
+    println(reverseString(s))  //вывод результата задачи 3_a_1
+    println(s.toLowerCase()) // задача  3_a_2
+    println(s.replace("!","")) // задача  3_a_3
+    println(s.replace("!","").concat(" and goodbye python!")) // задача  3_a_4
   }
+
+  // задача  3_a_1
+  def reverseString(inputString: String): String = {
+    val stringLength = inputString.length
+    var result = ""
+    for (i <- 0 until stringLength) {
+      result = inputString.charAt(i) + result
+    }
+    result
+  }
+
+
 }
